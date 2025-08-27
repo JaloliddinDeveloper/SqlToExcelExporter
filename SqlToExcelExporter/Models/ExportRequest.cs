2789c854a146dbeaa3935d6046a005230911ba9f
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2025-08-27 18:18:27 +0500

    Part-One

---
+++ SqlToExcelExporter/Models/ExportRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SqlToExcelExporter.Models
+{
+    public class ExportRequest
+    {
+        public string ConnectionString { get; set; }
+        public string SqlQuery { get; set; }
+        public string FileName { get; set; }
+    }
+}
